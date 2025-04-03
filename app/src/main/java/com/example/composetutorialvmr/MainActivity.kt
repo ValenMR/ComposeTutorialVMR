a5a7f3b8d2de64d307a610e200dcfc8e80c3a958
ValenMR <valentina.munozr1@udea.edu.co>
2025-04-02 21:18:38 -0500

    Lección2 terminada

--- app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
+++ app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
@@ -8,11 +8,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetutorialvmr.ui.theme.ComposeTutorialVMRTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +37,25 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Row{
+    //Añadiendo relleno alrededor de nuestro mensaje
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture"
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Tamaño de la imagen en 40 dp
+                .size(40.dp)
+                // poner la imagen en forman de círculo
+                .clip(CircleShape)
         )
+
+        // Añadiendo espacio horizontal entre la imagen y la column
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column{
             Text(text = msg.author)
+            // Añadiendo espacio vertical entre el author y el message text
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
