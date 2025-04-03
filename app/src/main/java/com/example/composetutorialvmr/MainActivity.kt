7aa7dc36dd7b8dd2dfdafb8902cd43b4ae5ee216
ValenMR <valentina.munozr1@udea.edu.co>
2025-04-02 20:57:38 -0500

    Lección2 con imagen importada

--- app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
+++ app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
@@ -4,34 +4,51 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetutorialvmr.ui.theme.ComposeTutorialVMRTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MessageCard("Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row{
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture"
+        )
+        Column{
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
+
+
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard(){
-    MessageCard("Android")
+    MessageCard(
+        msg = Message("VALENTINA MR", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+
 }
 
 @Composable
