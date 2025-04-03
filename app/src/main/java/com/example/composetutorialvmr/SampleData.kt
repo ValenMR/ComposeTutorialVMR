b72851c4e686ef23927d42719604aee4cb7447bf
ValenMR <valentina.munozr1@udea.edu.co>
2025-04-02 23:20:48 -0500

    Leccion4 cambios nombres

--- app/src/main/java/com/example/composetutorialvmr/SampleData.kt
+++ app/src/main/java/com/example/composetutorialvmr/SampleData.kt
@@ -7,11 +7,11 @@ object SampleData {
     // Sample conversation data
     val conversationSample = listOf(
         Message(
-            "Lexi",
-            "Test...Test...Test..."
+            "Juliana",
+            "Hello Valentina"
         ),
         Message(
-            "Lexi",
+            "Valentina",
             """List of Android versions:
             |Android KitKat (API 19)
             |Android Lollipop (API 21)
@@ -24,51 +24,51 @@ object SampleData {
             |Android 12 (API 31)""".trim()
         ),
         Message(
-            "Lexi",
-            """I think Kotlin is my favorite programming language.
+            "Felipe",
+            """I think Java is my favorite programming language.
             |It's so much fun!""".trim()
         ),
         Message(
-            "Lexi",
-            "Searching for alternatives to XML layouts..."
+            "Juliana",
+            "Searching for alternatives to Python..."
         ),
         Message(
-            "Lexi",
+            "Valentina",
             """Hey, take a look at Jetpack Compose, it's great!
             |It's the Android's modern toolkit for building native UI.
             |It simplifies and accelerates UI development on Android.
             |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
         ),
         Message(
-            "Lexi",
+            "Juliana",
             "It's available from API 21+ :)"
         ),
         Message(
-            "Lexi",
+            "Fernando",
             "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
         ),
         Message(
-            "Lexi",
+            "Valentina",
             "Android Studio next version's name is Arctic Fox"
         ),
         Message(
-            "Lexi",
+            "Valentina Cadena",
             "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
         ),
         Message(
-            "Lexi",
+            "Felipe",
             "I didn't know you can now run the emulator directly from Android Studio"
         ),
         Message(
-            "Lexi",
+            "Fernando",
             "Compose Previews are great to check quickly how a composable layout looks like"
         ),
         Message(
-            "Lexi",
+            "Valentina",
             "Previews are also interactive after enabling the experimental setting"
         ),
         Message(
-            "Lexi",
+            "Valentina Cadena",
             "Have you tried writing build.gradle with KTS?"
         ),
     )
