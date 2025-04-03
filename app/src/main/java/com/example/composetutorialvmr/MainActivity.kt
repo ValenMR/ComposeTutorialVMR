3ef9ed41802df46ae2407e5bf3ae278805358dc7
ValenMR <valentina.munozr1@udea.edu.co>
2025-04-02 21:54:45 -0500

    Leccion3 completada pero con un error

--- app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
+++ app/src/main/java/com/example/composetutorialvmr/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -15,20 +16,26 @@ import com.example.composetutorialvmr.ui.theme.ComposeTutorialVMRTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import android.content.res.Configuration
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MessageCard(Message("VALENTINA", "My Jetpack Compose"))
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("VALENTINA", "My Jetpack Compose"))
+                }
+            }
         }
     }
 }
@@ -47,28 +54,48 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // poner la imagen en forman de círculo
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
 
         // Añadiendo espacio horizontal entre la imagen y la column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column{
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             // Añadiendo espacio vertical entre el author y el message text
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.bodyMedium
+                )
         }
     }
 
 
 }
 
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
 @Preview
 @Composable
 fun PreviewMessageCard(){
-    MessageCard(
-        msg = Message("VALENTINA MR", "Hey, take a look at Jetpack Compose, it's great!")
-    )
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("VALENTINA", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 
 }
 
